7c0351b5d9fb92913855007b8ae3599094fc9db0
Brian Whitmer <brian.whitmer@gmail.com>
2020-04-01 10:12:13 -0600

    video adjusting, sidebar wip

---
+++ app/assets/javascripts/remote.js
@@ -0,0 +1,163 @@
+// To add a new handler, you will need to implement
+// a few methods as an object defined on the remote
+// object (for example, remote.myvideo)
+var remote = {
+  start_local_tracks: function() {
+    // Resolves a list of objects with the following attributes
+    // { type: "video, audio, data", id: "", generate_dom: function...}
+    return remote[remote.backend].start_local_tracks();
+  },
+  send_message: function(room_id, message) {
+    // Resolves with the following attributes
+    // { message: [Message Object] }
+    return remote[remote.backend].send_message(message);
+  },
+  connect_to_remote: function(token, room_key) {
+    // Resolves an object with the following attributes
+    // { id: "", 
+    return remote[remote.backend].connect_to_remote(token, room_key);
+  },
+  user_added: function(room, user) {
+    // Trigger for each user that joins, or for each
+    // user that is already in the session
+    remote.notify('user_added', {
+      user: user,
+      room_id: room.id
+    })
+  },
+  track_added: function(room, user, track) {
+    // Trigger for each track that is added for a remote user
+    remote.notify('track_added', {
+      track: track,
+      room_id: room.id,
+      user_id: user.id
+    });
+  },
+  message_recieved: function(room, user, track, message) {
+    // Trigger for each data track message receiveds
+    var json = message;
+    try {
+      json = JSON.parse(str);
+    } catch(e) { }
+  
+    remote.notify('message', {
+      message: json,
+      room_id: room.id,
+      track_id: track.id,
+      user_id: user.id
+    });
+  },
+  // Helper methods that you can ignore
+  notify: function(message_type, payload) {
+    var listeners = (remote.listeners || {})[message_type] || [];
+    listeners.forEach(function(listener) {
+      listener(payload);
+    })
+  },
+  handle_listener: function(message_type, callback, add) {
+    var listeners = (remote.listeners || {})[message_type] || [];
+    listeners.forEach(function(listener) {
+      listener(payload);
+    })
+    listeners = listeners.filter(function(l) { return l != callback; });
+    if(add) {
+      listeners.push(callback);
+    }
+    remote.listeners = remote.listeners || {};
+    remote.listeners[message_type] = listeners;
+  },
+  addEventListener: function(message_type, callback) {
+    remote.handle_listener(message_type, callback, true);
+  },
+  removeEventListener: function(messsage_type, callback) {
+    remote.handle_listener(message_type, callback, false);
+  }
+};
+remote.twilio = {
+  start_local_tracks: function() {
+    return new Promise(function(res, rej) {
+      var local_track = new Twilio.Video.LocalDataTrack();
+      remote.twilio.data_track = local_track;
+      Twilio.Video.createLocalTracks({
+        audio: true,
+        video: true
+      }).then(function(tracks) {
+        tracks.push(local_track);
+        remote.twilio.local_tracks = tracks;
+        var result = [];
+        tracks.forEach(function(track) {
+          result.push({
+            type: track.kind,
+            id: track.name,
+            generate_dom: track.attach ? function() { return track.attach(); } : null,
+          });
+        });
+        res(result);
+      }, function(err) {
+        rej(err);
+      })
+    });
+  },
+  connect_to_remote: function(token, room_key) {
+    return new Promise(function(res, rej) {
+      Twilio.Video.connect(token, { name:room_key, tracks: remote.twilio.local_tracks }).then(function(room) {
+        remote.twilio.rooms = remote.twilio.rooms || {};
+        remote.twilio.rooms[room_key] = room;
+        var room_ref = {
+          id: room_key
+        };
+        res(room_ref);
+        var track_participant = function(participant) {
+          var participant_ref = {
+            id: participant.ident
+          };
+          remote.user_added(participant_ref);
+          var add_track = function(track) {
+            // TODO: can we figure out the video dimensions here?
+            remote.track_added(room_ref, participant_ref, {
+              type: track.kind,
+              id: track.name,
+              generate_dom: track.attach ? function() { return track.attach(); } : null
+            });
+            track.on('message', function(data) {
+              remote.message_recieved(room_ref, participant_ref, track, data);
+            });
+          };
+          participant.tracks.forEach(function(publication) {
+            if (publication.isSubscribed) {
+              add_track(track);
+            }
+          });
+
+          participant.on('trackSubscribed', function(track) {
+            add_track(track);
+          });  
+        };
+        setTimeout(function() {
+          room.participants.forEach(function(participant) {
+            track_participant(participant);
+          });
+        }, 100);
+        room.on('participantConnected', function(participant) {
+          console.log("A remote Participant connected: " + participant);
+          track_participant(participant);
+        });  
+      }, function(err) {
+        rej(err);
+      });
+    });
+  },
+  send_message: function(message) {
+    return new Promise(function(res, rej) {
+      if(remote.twilio.data_track) {
+        var str = message;
+        if(typeof(str) != 'string') { str = JSON.stringify(str); }
+        remote.twilio.data_track.send(str);
+        res({sent: message});
+      } else {
+        rej({error: "no data track found"});
+      }
+    });
+  }
+};
+remote.backend = 'twilio';
--- app/assets/javascripts/room.js
+++ app/assets/javascripts/room.js
@@ -1,110 +1,127 @@
+remote.addEventListener('track_added', function(data) {
+  var track = data.track;
+  if(track.generate_dom) {
+    console.log("adding track", track);
+    if(track.type == 'video' || track.type == 'audio') {
+      var priors = document.getElementById('partner').getElementsByTagName(track.type);
+      for(var idx = 0; idx < priors.length; idx++) {
+        priors[idx].parentNode.removeChild(priors[idx]);
+      }
+    }
+    var elem = track.generate_dom();
+    document.getElementById('partner').appendChild(elem);
+    if(track.type == 'video') {
+      setTimeout(function() {
+        room.size_video();
+      }, 500);
+      room.current_video_id = track.id;
+    }
+  }
+});
+remote.addEventListener('user_added', function(data) {
+  // TODO: keep a rotation of helpers for the communicator,
+  // and keep communicators on everyone else's view
+});
+remote.addEventListener('user_removed', function(data) {
+});
+remote.addEventListener('message', function(data) {
+  room.handle_message(data);
+});
+var zoom_factor = 1.1;
 var room = {
+  size_video: function() {
+    var box = document.getElementById('partner');
+    var elem = box && box.getElementsByTagName('VIDEO')[0];
+    if(box && elem) {
+      var rect = box.getBoundingClientRect();
+      var bw = rect.width;
+      var bh = rect.height;
+      var zoom = room.zoom_level || 1.0;
+      var vw = elem.videoWidth;
+      var vh = elem.videoHeight;  
+      var xscale = bw / vw;
+      var yscale = bh / vh;
+      if(vw * zoom < bw && vh * zoom < bh) {
+        room.zoom_level = room.zoom_level * zoom_factor;
+        return room.size_video();
+      }
+      var scale = Math.max(xscale, yscale);
+      elem.style.width = (vw * scale * zoom) + "px";
+      elem.style.height = (vh * scale * zoom) + "px";
+      var fudge_x = (((vw * scale * zoom) - bw) / -2);
+      var fudge_y = (((vh * scale * zoom) - bh) / -2);
+      var shift_x = 0;
+      var shift_y = 0;
+      if(room.shift_x && fudge_x < 0) {
+        shift_x = Math.max(Math.min(room.shift_x || 0, -1 * fudge_x), fudge_x);
+      }
+      if(room.shift_y && fudge_y < 0) {
+        shift_y = Math.max(Math.min(room.shift_y || 0, -1 * fudge_y), fudge_y);  
+      }
+      console.log("SCALE", fudge_x, fudge_y, shift_x, shift_y);
+      elem.style.marginLeft = (fudge_x + shift_x) + "px";
+      elem.style.marginTop = (fudge_y + shift_y) + "px";
+    }
+  },
+  zoom: function(zoom_in) {
+    room.zoom_level = (room.zoom_level || 1.0);
+    if(zoom_in) {
+      room.zoom_level = room.zoom_level * zoom_factor;
+    } else {
+      room.zoom_level = room.zoom_level / zoom_factor;
+    }
+    room.size_video();
+  },
+  send_image: function(image_url, alt) {
+    if(!room.current_room) { return; }
+    remote.send_message(room.current_room.id, {
+      action: 'image',
+      url: image_url,
+      text: alt
+    })
+    room.show_image(image_url, alt, false);
+  },
+  send_update: function() {
+    if(room.update_timeout) {
+      clearTimeout(room.update_timeout);
+      room.update_timeout = null;
+    }
+    if(!room.current_room) { return; }
+    var track_ids = [];
+    for(var idx = 0; idx < (room.local_tracks || []).length; idx++) {
+      track_ids.push(room.local_tracks[idx].id);
+    }
+    remote.send_message(room.current_room.id, {
+      user_id: room.current_room.user_id,
+      tracks: track_ids
+    });
+    room.update_timeout = setTimeout(function() {
+      room.send_update();
+    }, 5000);
+  },
   start: function() {
     var room_id = (location.pathname.match(/\/rooms\/(\w+)/) || {})[1];
-    var local_track = new Twilio.Video.LocalDataTrack();
-    _room.local_track = local_track;
-    var got_message = function(str) {
-      var json = null;
-      try {
-        json = JSON.parse(str);
-      } catch(e) { }
-      if(json && json.action == 'click') {
-        var button = (room.buttons || []).find(function(b) { return b.id == json.button.id; });
-        if(button && button.cell) {
-          button.cell.classList.add('highlight');
-          setTimeout(function() {
-            button.cell.classList.remove('highlight');
-          }, 5000);
-        }
-        room.buttons.forEach(function(button) {
-
-        })
-      } else {
-        console.log("MESSAGE:", json);
-      }
-    };
     var enter_room = function() {
       session.ajax('/api/v1/rooms/' + room_id, {
         method: 'PUT',
         data: {user_id: localStorage.user_id || room_id} 
       }).then(function(res) {
-        Twilio.Video.createLocalTracks({
-          audio: true,
-          video: true
-        }).then(function(tracks) {
-          tracks.push(local_track);
-          Twilio.Video.connect(res.access_token, { name:res.room.key, tracks: tracks }).then(function(room) {
-            console.log("Successfully joined a Room: " + room + " as " + res.user_id);
-            if(room_id == res.user_id) {
-              // Default Order
-              var grid = document.getElementsByClassName('grid')[0];
-              var cells = grid.getElementsByClassName('cell');
-              for(var idx = 0; idx < cells.length; idx++) {
-                var num = parseInt(cells[idx].getAttribute('data-idx'), 10);
-                cells[idx].innerText = _room.buttons[num].text;
-                cells[idx].style.visibility = 'visible';
-                cells[idx].button = _room.buttons[num];
-                _room.buttons[num].cell = cells[idx];
-              }
-              var again = function() {
-                local_track.send(JSON.stringify({a: 1, b: 'ok'}));
-                setTimeout(function() {
-                  again();
-                }, 1000);
-              };
-              again();
-            } else {
-              // Reverse Order
-              var grid = document.getElementsByClassName('grid')[0];
-              var cells = grid.getElementsByClassName('cell');
-              var new_order = [].concat(_room.buttons);
-              new_order[0] = _room.buttons[2];
-              new_order[2] = _room.buttons[0];
-              new_order[3] = _room.buttons[4];
-              new_order[4] = _room.buttons[3];
-              new_order[5] = _room.buttons[7];
-              new_order[7] = _room.buttons[5];
-              for(var idx = 0; idx < cells.length; idx++) {
-                var num = parseInt(cells[idx].getAttribute('data-idx'), 10);
-                cells[idx].innerText = new_order[num].text;
-                cells[idx].style.visibility = 'visible';
-                cells[idx].button = new_order[num];
-                new_order[num].cell = cells[idx];
-              }
+        remote.start_local_tracks().then(function(tracks) {
+          for(var idx = 0; idx < tracks.length; idx++) {
+            if(tracks[idx].type == 'video') {
+              document.getElementById('communicator').innerHTML = "";
+              document.getElementById('communicator').appendChild(tracks[idx].generate_dom());
             }
-            var track_participant = function(participant) {
-              var add_track = function(track) {
-                if(track.attach) {
-                  console.log("adding track", track);
-                  if(track.kind == 'video' || track.kind == 'audio') {
-                    var priors = document.getElementById('partner').getElementsByTagName(track.kind);
-                    for(var idx = 0; idx < priors.length; idx++) {
-                      priors[idx].parentNode.removeChild(priors[idx]);
-                    }
-                  }
-                  document.getElementById('partner').appendChild(track.attach());
-                }
-                track.on('message', function(data) {
-                  got_message(data);
-                });
-              };
-              participant.tracks.forEach(function(publication) {
-                if (publication.isSubscribed) {
-                  add_track(publication.track);
-                }
-              });
-  
-              participant.on('trackSubscribed', function(track) {
-                add_track(track);
-              });            
-            }
-            room.participants.forEach(function(participant) {
-              track_participant(participant);
-            });
-            room.on('participantConnected', function(participant) {
-              console.log("A remote Participant connected: " + participant);
-              track_participant(participant);
-            });
+          }
+          remote.connect_to_remote(res.access_token, res.room.key).then(function(room_session) {
+            console.log("Successfully joined a Room: " + room_session.id + " as " + res.user_id);
+            room_session.user_id = res.user_id;
+            room_session.communicator_id = room_id;
+            room_session.for_self = room_id == res.user_id;
+            room.current_room = room_session;
+            room.local_tracks = tracks;
+            room.send_update();
+            room.show_grid(room_session.for_self);
           }, function(error) {
             console.error("Unable to connect to Room: " + error.message);
           });
@@ -127,14 +144,223 @@ var room = {
     } else {
       enter_room();
     }
+  },
+  show_grid: function(for_communicator) {
+    if(for_communicator) {
+      // Default Order
+      var grid = document.getElementsByClassName('grid')[0];
+      var cells = grid.getElementsByClassName('cell');
+      for(var idx = 0; idx < cells.length; idx++) {
+        var num = parseInt(cells[idx].getAttribute('data-idx'), 10);
+        cells[idx].innerText = room.buttons[num].text;
+        cells[idx].style.visibility = 'visible';
+        cells[idx].button = room.buttons[num];
+        room.buttons[num].cell = cells[idx];
+      }
+    } else {
+      // Reverse Order
+      var grid = document.getElementsByClassName('grid')[0];
+      var cells = grid.getElementsByClassName('cell');
+      var new_order = [].concat(room.buttons);
+      new_order[0] = room.buttons[2];
+      new_order[2] = room.buttons[0];
+      new_order[3] = room.buttons[4];
+      new_order[4] = room.buttons[3];
+      new_order[5] = room.buttons[7];
+      new_order[7] = room.buttons[5];
+      for(var idx = 0; idx < cells.length; idx++) {
+        var num = parseInt(cells[idx].getAttribute('data-idx'), 10);
+        cells[idx].innerText = new_order[num].text;
+        cells[idx].style.visibility = 'visible';
+        cells[idx].button = new_order[num];
+        new_order[num].cell = cells[idx];
+      }
+    }
+  },
+  show_image: function(url, text, big_image) {
+    room.image_slots = room.image_slots || [];
+    room.image_slots.index = room.image_slots.index || 0;
+    var found_empty = false;
+    for(var idx = 0; idx < 3; idx++) {
+      if(!room.image_slots[idx] && !found_empty) {
+        found_empty = true;
+        room.image_slots.index = idx;
+      }
+    }
+    var idx = room.image_slots.index;
+    var img = document.createElement('img');
+    img.classList.add('reaction');
+    img.style.left = ((idx * 60) + 10) + "px";
+    img.src = url;
+    img.alt = text;
+    var wait = 10;
+    if(room.image_slots[idx]) {
+      var prior = room.image_slots[idx];
+      prior.style.opacity = 0;
+      setTimeout(function() {
+        prior.parentNode.removeChild(prior);
+      }, 1000);
+      wait = 510;
+    }
+    room.image_slots[idx] = img;
+    room.image_slots.index = (idx + 1) % 3;
+    setTimeout(function() {
+      var complete = function() {
+        if(room.image_slots[idx] == img) {
+          room.image_slots[idx] = null;
+        }
+        img.style.opacity = 0;
+        setTimeout(function() {
+          if(img.parentNode) {
+            img.parentNode.removeChild(img);
+          }
+        }, 500);
+      };
+      document.getElementsByClassName('preview')[0].appendChild(img);
+      if(!big_image) {
+        img.classList.add('finished');
+        setTimeout(complete, 10000);
+      } else {
+        setTimeout(function() {
+          img.classList.add('finished');
+          setTimeout(complete, 20000);
+        }, 3000);
+      }
+      setTimeout(function() {
+        img.style.opacity = 1;
+      }, 100);
+    }, wait);
+  },
+  toggle_zoom: function(force) {
+    var $nav = $("#nav");
+    if(force == null) {
+      force = $nav.css('opacity') == '1' ? false : true;
+    }
+    var now = (new Date()).getTime();
+    if(!force && $nav[0].shown_at && $nav[0].shown_at > now - 500) {
+      return;
+    }
+    $nav.css('opacity', force ? 1 : 0);
+    $nav[0].shown_at = now;
+    $nav[0].hide_at = now + 5000;
+    if(!room.nav_interval) {
+      room.nav_interval = setInterval(function() {
+        var now = (new Date()).getTime();
+        var hide_at = ($("#nav")[0] || {}).hide_at;
+        if(hide_at && hide_at < now) {
+          $("#nav").css('opacity', 0);
+          $("#nav")[0].hide_at = null;
+        }
+      }, 500);
+    }
+  },
+  handle_message: function(data) {
+    var json = data.message;
+    if(!room.current_room || data.user_id == room.current_room.user_id) { return; }
+    if(json && json.action == 'click') {
+      var button = (room.buttons || []).find(function(b) { return b.id == json.button.id; });
+      if(button && button.cell) {
+        button.cell.classList.add('highlight');
+        setTimeout(function() {
+          button.cell.classList.remove('highlight');
+        }, 5000);
+      }
+      room.buttons.forEach(function(button) {
+  
+      });
+    } else if(json && json.action == 'image') {
+      var big_image = false;
+      if(data.user_id == room.current_room.communicator_id && !room.current_room.for_self) {
+        // if sent by the communicator, who is not you
+        // show a big version of the image
+        big_image = true;
+      } else if(data.user_id != room.current_room.communicator_id && room.current_room.for_self) {
+        // or if sent by someone else and you are the communicator
+        // show a big version of the image
+        big_image = true;
+      } else {
+        // show a small version of the image
+      }
+      room.show_reaction(json.url, json.text, big_image);
+    } else {
+      // TODO: if more users in the feed, ensure
+      // that everyone else sees the communicator's video feed
+      console.log("MESSAGE:", json);
+    }  
   }
 };
-var _room = room;
+window.addEventListener('resize', function() {
+  room.size_video();
+});
+var shift = function(event) {
+  room.shift_x = event.clientX - (room.drag_x || event.clientX);
+  room.shift_y = event.clientY - (room.drag_y || event.clientY);
+  room.size_video();
+  room.toggle_zoom(true);
+  console.log("drag", room.shift_x, room.shift_y);
+}
+var drag = function(event) {
+  room.drag_x = event.clientX - (room.shift_x || 0);
+  room.drag_y = event.clientY - (room.shift_y || 0);
+};
+document.addEventListener('mousemove', function(event) {
+  if($(event.target).closest('#partner').length > 0 && event.buttons == 1) {
+    event.preventDefault();
+    shift(event);
+  }
+});
+document.addEventListener('touchmove', function(event) {
+  if($(event.target).closest('#partner').length > 0) {
+    event.preventDefault();
+    shift(event);
+  }
+
+});
+document.addEventListener('mousedown', function(event) {
+  if($(event.target).closest('#partner').length > 0) {
+    event.preventDefault();
+    drag(event);
+  }
+});
+document.addEventListener('touchstart', function(event) {
+  if($(event.target).closest('#partner').length > 0) {
+    event.preventDefault();
+    drag(event);
+  }
+});
 document.addEventListener('click', function(event) {
-  var $elem = $(event.target).closest('.cell');
-  if($elem.length > 0) {
-    if(room.local_track) {
-      room.local_track.send(JSON.stringify({action: 'click', button: {id: $elem[0].button.id }}));
+  var $cell = $(event.target).closest('.cell');
+  var $button = $(event.target).closest('.button');
+  var $partner = $(event.target).closest('#partner');
+  var $zoom = $(event.target).closest('.zoom');
+  if($(event.target).closest("#nav").css('opacity') == '0') {
+    $partner = $("#partner");
+    $zoom.blur();
+  }
+  if($partner.length > 0) {
+    room.toggle_zoom();
+  } else if($cell.length > 0) {
+    remote.send_message({action: 'click', button: {id: $cell[0].button.id }});
+    $cell.addClass('my_highlight');
+    setTimeout(function() {
+      $cell.removeClass('my_highlight');
+    }, 1000);
+  } else if($button.length > 0) {
+    var action = $button.attr('data-action');
+    if(action == 'end') {
+      alert('done!');
+    } else if(action == 'send') {
+      room.send_image("https://d18vdu4p71yql0.cloudfront.net/libraries/twemoji/1f339.svg", "rose");
+    }
+  } else if($zoom.length > 0) {
+    event.preventDefault();
+    $zoom.blur();
+    $("#nav")[0].hide_at = (new Date()).getTime() + 10000;
+    if($("#nav").css('opacity') != '1') { return; }
+    if($zoom.attr('data-direction') == 'in') {
+      room.zoom(true);
+    } else {
+      room.zoom(false);
     }
   }
 });
